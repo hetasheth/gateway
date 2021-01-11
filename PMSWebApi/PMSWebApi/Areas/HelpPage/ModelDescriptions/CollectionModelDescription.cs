39b4d2b642eaa327b2cd2c00337a87d90c4b2c72
Heta Sheth <hetasheth710@gmail.com>
2021-01-11 21:54:24 +0530

    Compledeted Product Mangement System(Trainee Assignment)

--- PMSWebApi/PMSWebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace PMSWebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- PMSWebApi/PMSWebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace PMSWebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
